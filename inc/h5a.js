c53f5c48d11c97b43269ab969d171fcede2beb53
Chad Sturtz <sturtz70@gmail.com>
2014-10-24 08:52:04 -0700

    copyright and license header in h5a.js

--- inc/h5a.js
+++ inc/h5a.js
@@ -1,3 +1,24 @@
+/*
+ * ******************************************************************************
+ *  file: h5a.js
+ *
+ *  Original Work from jQuery Plugin Copyright (c) 2001-2014. Matteo Bicocchi (Pupunzi);
+ *  Open lab srl, Firenze - Italy
+ *  email: matteo@open-lab.com
+ *  site: 	http://pupunzi.com
+ *  blog:	http://pupunzi.open-lab.com
+ * 	http://open-lab.com
+ *
+ *  New and Modified Work for h5a.js Copyright (c) 2014. Chad Sturtz;
+ *  email: sturtz70@gmail.com
+ *
+ *  Licences: MIT
+ *  http://www.opensource.org/licenses/mit-license.php
+ *  
+ *  *****************************************************************************
+ *
+ */
+
 ;(function(){
 	var   ua
 		, isAndroid
@@ -18,5 +39,5 @@
 	isDevice = 'ontouchstart' in window;
 	isChrome = "chrome" in window;
 	isMoz = "mozAnimationStartTime" in window;
-	
+
 })();
